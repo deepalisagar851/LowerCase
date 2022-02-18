103bea71e8f180f59044d7760f3b9a78bd47ffa2
Deepali Sagar <Deepali Sagar@DESKTOP-JLEC72J>
2022-02-18 14:32:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter a String :");
+            str = Console.ReadLine();
+            Console.WriteLine("Converted String is :" + str.ToLower());
+
+        }
+    }
+}
+
